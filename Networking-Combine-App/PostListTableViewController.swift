80153b19fa29fa8f198a986e80493c26605b2d47
Oscar David Myerston Vega <oscar.myerston@dev275.local>
2023-02-16 16:53:38 -0500

    first commit

--- Networking-Combine-App/PostListTableViewController.swift
+++ Networking-Combine-App/PostListTableViewController.swift
@@ -8,11 +8,26 @@
 
 import Foundation
 import UIKit
+import Combine
 
 class PostListTableViewController: UITableViewController {
     
+    private var webservice = Webservice()
+    private var cancellable: AnyCancellable?
+    
+    private var posts = [Post]() {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.cancellable = self.webservice.getPosts()
+            .catch { _ in Just(self.posts)}
+            .assign(to: \.posts, on: self)
+        
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -20,12 +35,16 @@ class PostListTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return self.posts.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell", for: indexPath)
+        
+        let post = self.posts[indexPath.row]
+        cell.textLabel?.text = post.title
+        
         return cell
     }
     
